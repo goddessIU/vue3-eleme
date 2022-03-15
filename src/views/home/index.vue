68f94b9af62289b54a33c74452453d44f8a8a55c
goddessIU <2581606394@qq.com>
2022-03-15 10:57:54 +0800

    fix

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,13 +1,13 @@
 <template>
     <HomeHeader />
-    <ServiceBar />
-    <SnapUp />
+    <!-- <ServiceBar /> -->
+    <!-- <SnapUp /> -->
     <Recommend />
 </template>
 
 <script setup scoped>
 import HomeHeader from '../../components/HomeHeader.vue';
-import SnapUp from '../../components/SnapUp.vue';
-import Recommend from '../../components/Recommend.vue';
+// import SnapUp from '../../components/SnapUp.vue';
+// import Recommend from '../../components/Recommend.vue';
 import ServiceBar from '../../components/ServiceBar.vue'
 </script>
